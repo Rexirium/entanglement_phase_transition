ea373c7b2b4c16e2e841377317b170a30bb98b77
Rexirium <rexirium@qq.com>
2025-08-01 21:30:03 +0800

    time evolution completed

--- time_evolution.jl
+++ time_evolution.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra
 using ITensors, ITensorMPS
+include("entropies.jl")
 
 function ITensors.op(::OpName"RdU", ::SiteType"S=1/2", s::Index...; eltype=ComplexF64)
     d = prod(dim.(s))
@@ -24,11 +25,11 @@ function nonHermitianGenerator(eta::Float64, s0::Index)
     return op(M, s0)
 end
 
-function mps_evolve(psi0::MPS, time::Int, prob::Real, eta::Real; cutoff::Real=1e-14)
+function mps_evolve(psi0::MPS, ttotal::Int, prob::Real, eta::Real; cutoff::Real=1e-14)
     psi = copy(psi0)
     sites = siteinds(psi)
     Ls= length(sites)
-    for t in 1:time
+    for t in 1:ttotal
         start = isodd(t) ? 1 : 2
         for j in start:2:Ls-1
             s1, s2 = sites[j], sites[j+1]
@@ -48,9 +49,41 @@ function mps_evolve(psi0::MPS, time::Int, prob::Real, eta::Real; cutoff::Real=1e
     return psi
 end
 
+function entropy_evolve(psi0::MPS, ttotal::Int, prob::Real, eta::Real, b::Int, which_ent::Real; 
+     cutoff::Real=1e-14, ent_cutoff::Real=1e-12)
+    psi = copy(psi0)
+    sites = siteinds(psi) 
+    Ls = length(sites)
+    entropies = Float64[]
+    for t in 1:ttotal
+        start = isodd(t) ? 1 : 2
+        for j in start:2:Ls-1
+            s1, s2 = sites[j], sites[j+1]
+            U = op("RdU", s1, s2)
+            psi = apply(U, psi; cutoff)
+        end
+        for j in 1:Ls
+            samp = rand()
+            if samp < prob
+                s = sites[j]
+                M = op("NH", s; eta=eta)
+                psi = apply(M, psi; cutoff)
+            end
+        end
+        normalize!(psi)
+        entropy = Renyi_entropy(psi, b, which_ent; cutoff=ent_cutoff)
+        push!(entropies, entropy)
+    end
+    return psi, entropies
+end
+
 let 
-    ss = siteinds("S=1/2", 4)
-    psi = random_mps(ss; linkdims=2)
-    o = op("NH", ss[1]; eta=0.5)
-    array(o)
+    L = 8
+    T = 2L
+    b = L÷2
+    p, eta = 0.5, 0.5
+    ss = siteinds("S=1/2", L)
+    psi0 = MPS(ss, "Up")
+    psi, entropies = entropy_evolve(psi0, T, p, eta, b, 1)
+    println(entropies)
 end
