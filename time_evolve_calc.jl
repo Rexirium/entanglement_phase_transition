3c4badd0075e1c265100f01731dafa3f39425762
Rexirium <rexirium@qq.com>
2025-08-06 08:47:26 +0800

    time evolution plot

---
+++ time_evolve_calc.jl
@@ -0,0 +1,69 @@
+using HDF5
+include("time_evolution.jl")
+
+let 
+    L = 10
+    T, b = 4L, L ÷ 2
+    ps = 0.0:0.2:1.0
+    ηs = 0.0:0.5:2.0
+    numsamp = 10
+
+    ss = siteinds("S=1/2", L)
+    psi0 = MPS(ss, "Up")
+
+    prob_evolves = []
+    prob_distris = []
+
+    for p in ps
+        evolvesamp = []
+        distrisamp = []
+        for _ in 1:numsamp
+            psi, evolve = entropy_evolve(psi0, T, p, 0.5, b, 1)
+            distri = [Renyi_entropy(psi, x, 1) for x in 0:L]
+            push!(evolvesamp, evolve)
+            push!(distrisamp, distri)
+        end
+
+        meanevolve = sum(evolvesamp)/numsamp
+        meandistri = sum(distrisamp)/numsamp
+
+        push!(prob_evolves, meanevolve)
+        push!(prob_distris, meandistri)
+    end
+    prob_evolves = hcat(prob_evolves...)
+    prob_distris = hcat(prob_distris...)
+
+    eta_evolves = []
+    eta_distris = []
+
+    for η in ηs
+        evolvesamp = []
+        distrisamp = []
+        for _ in 1:numsamp
+            psi, evolve = entropy_evolve(psi0, T, 0.5, η, b, 1)
+            distri = [Renyi_entropy(psi, x, 1) for x in 0:L]
+            push!(evolvesamp, evolve)
+            push!(distrisamp, distri)
+        end
+
+        meanevolve = sum(evolvesamp)/numsamp
+        meandistri = sum(distrisamp)/numsamp
+
+        push!(eta_evolves, meanevolve)
+        push!(eta_distris, meandistri)
+    end
+    eta_evolves = hcat(eta_evolves...)
+    eta_distris = hcat(eta_distris...) 
+
+    h5open("timeevolvedata.h5", "w") do file
+        write(file, "ps", collect(ps))
+        write(file, "ηs", collect(ηs))
+        write(file, "L", L)
+        write(file, "T", T)
+        write(file, "prob_evolves", prob_evolves)
+        write(file, "prob_distris", prob_distris)
+        write(file, "eta_evolves", eta_evolves)
+        write(file, "eta_distris", eta_distris)
+    end
+end
+
--- time_evolve_plot.jl
+++ time_evolve_plot.jl
@@ -1,81 +1,52 @@
-using Plots
-include("time_evolution.jl")
+using Plots, LaTeXStrings
+default(
+    grid=false, 
+    titlelocation=:left,
+    framestyle=:box,
+    legend=:topright,
+    guidefontsize=14,
+    legendfontsize=10,
+    tickfontsize=10,
+    bottommargin=2Plots.mm,
+    leftmargin=4Plots.mm
+)
 
 let 
-    L = 10
-    T, b = 4L, L ÷ 2
-    ps = 0.0:0.2:1.0
-    ηs = 0.0:0.5:2.0
-    numsamp = 10
-
-    ss = siteinds("S=1/2", L)
-    psi0 = MPS(ss, "Up")
-
-    prob_evolves = []
-    prob_distris = []
-
-    for p in ps
-        evolvesamp = []
-        distrisamp = []
-        for _ in 1:numsamp
-            psi, evolve = entropy_evolve(psi0, T, p, 0.5, b, 1)
-            distri = [Renyi_entropy(psi, x, 1) for x in 0:L]
-            push!(evolvesamp, evolve)
-            push!(distrisamp, distri)
-        end
-
-        meanevolve = sum(evolvesamp)/numsamp
-        meandistri = sum(distrisamp)/numsamp
-
-        push!(prob_evolves, meanevolve)
-        push!(prob_distris, meandistri)
-    end
-    prob_evolves = hcat(prob_evolves...)
-    prob_distris = hcat(prob_distris...)
-
-    eta_evolves = []
-    eta_distris = []
-
-    for η in ηs
-        evolvesamp = []
-        distrisamp = []
-        for _ in 1:numsamp
-            psi, evolve = entropy_evolve(psi0, T, 0.5, η, b, 1)
-            distri = [Renyi_entropy(psi, x, 1) for x in 0:L]
-            push!(evolvesamp, evolve)
-            push!(distrisamp, distri)
-        end
-
-        meanevolve = sum(evolvesamp)/numsamp
-        meandistri = sum(distrisamp)/numsamp
-
-        push!(eta_evolves, meanevolve)
-        push!(eta_distris, meandistri)
-    end
-    eta_evolves = hcat(eta_evolves...)
-    eta_distris = hcat(eta_distris...) 
+    file = h5open("timeevolvedata.h5", "r")
+    ps = read(file, "ps")
+    ηs = read(file, "ηs")
+    T = read(file, "T")
+    L = read(file, "L")
+    prob_evolves = read(file, "prob_evolves")
+    prob_distris = read(file, "prob_distris")
+    eta_evolves = read(file, "eta_evolves")
+    eta_distris = read(file, "eta_distris")
+    close(file)
 
     pt = plot(0:T, prob_evolves, lw = 2,
-         xlabel="time", ylabel="entanglement entropy", 
+         xlabel=L"t", ylabel=L"S_1", 
          title="entanglement entropy evolution for varying p",
-         label=string.(collect(ps)'), legend=:topright, framestyle=:box)
+         label=string.(collect(ps)'),
+         legend_title=L"p")
     
     px = plot(0:L, prob_distris, lw=2,
-         xlabel="bipartition", ylabel="entanglement entropy", 
+         xlabel=L"x", ylabel=L"S_1", 
          title="entanglement entropy distribution for varying p",
-         label=string.(collect(ps)'), legend=:topright, framestyle=:box)
+         label=string.(collect(ps)'),
+         legend_title=L"p")
 
     et = plot(0:T, eta_evolves, lw=2,
-         xlabel="time", ylabel="entanglement entropy",
+         xlabel=L"t", ylabel=L"S_1",
          title="entanglement entropy evolution for varying η",
-         label=string.(collect(ηs)'), legend=:topright, framestyle=:box)
+         label=string.(collect(ηs)'),
+         legend_title=L"\eta")
 
     ex = plot(0:L, eta_distris, lw=2,
-         xlabel="bipartition", ylabel="entanglement entropy",
+         xlabel=L"x", ylabel=L"S_1",
          title="entanglement entropy distribution for varying η",
-         label=string.(collect(ηs)'), legend=:topright, framestyle=:box)
+         label=string.(collect(ηs)'),
+         legend_title=L"\eta")
 
     plot(pt, px, et, ex, layout=(2,2), size=(1200, 800))
 
 end
-
