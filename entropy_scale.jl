db4baa9bd07159f8d33eef5cfb11fc80745d767a
Rexirium <rexirium@qq.com>
2025-08-26 04:54:01 +0800

    print running infos

--- entropy_scale.jl
+++ entropy_scale.jl
@@ -18,13 +18,17 @@ let
         tt, b = 4l, l ÷ 2
 
         for i in 1:nprob
+            p = ps[i]
             prob_scales_mean[i,j], prob_scales_std[i,j] = 
-                entropy_mean(l, tt, ps[i], η0, b; numsamp=10, retstd=true)
+                entropy_mean(l, tt, p, η0, b; numsamp=10, retstd=true)
+            println("L=$l, p=$(round(p,digits=2)), η=0.5 done")
         end
 
         for i in 1:neta
+            η = ηs[i]
             eta_scales_mean[i,j], eta_scales_std[i,j] = 
-                entropy_mean(l, tt, p0, ηs[i], b; numsamp=10, retstd=true)
+                entropy_mean(l, tt, p0, η, b; numsamp=10, retstd=true)
+            println("L=$l, p=0.50, η=$(round(η,digits=2)) done")
         end
     end
 
