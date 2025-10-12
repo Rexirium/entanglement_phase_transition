2d61df474563ae701bdf82014af9d25b023750a4
Rexirium <rexirium@qq.com>
2025-10-12 14:20:11 +0800

    add test for core calculation

--- entropy_calc.jl
+++ entropy_calc.jl
@@ -122,15 +122,3 @@ function entropy_mean_multi(lsize::Int, ttotal::Int, prob::Real, para::Tuple{Rea
     end
 end
 
-if abspath(PROGRAM_FILE) == @__FILE__
-# let
-    # example usage
-    L = 12
-    T, b = 4L, L ÷ 2
-    prob = 0.5
-    eta = 0.5
-    numsamp = 10
-
-    @timev  entropy_mean(L, T, prob, eta; numsamp=numsamp, retstd=true)
-    @timev  entropy_mean_multi(L, T, prob, eta; numsamp=numsamp, retstd=true)
-end
