8fda48730b0fa5f2a6fcaf96148039f0afbc2ab4
Rexirium <rexirium@qq.com>
2025-10-16 19:55:05 +0800

    Lmax = 16 another plot

--- entropy_plot.jl
+++ entropy_plot.jl
@@ -3,7 +3,7 @@ using HDF5
 default(
     grid=false, 
     titlelocation=:left,
-    titlefontsize=14,
+    titlefontsize=12,
     framestyle=:box,
     legend=:topright,
     guidefontsize=12,
@@ -15,10 +15,6 @@ default(
 
 let 
     # Read data from HDF5 file
-    prob_scales_mean = Matrix{Float64}(undef, nprob, nL)
-    prob_scales_std = Matrix{Float64}(undef, nprob, nL)
-    eta_scales_mean = Matrix{Float64}(undef, neta, nL)
-    eta_scales_std = Matrix{Float64}(undef, neta, nL)
 
     file = h5open("data/entropy_scale_data.h5", "r")
 
@@ -27,7 +23,16 @@ let
     ηs = read(file, "params/ηs")
     p0 = read(file, "params/p0")
     η0 = read(file, "params/η0")
+    close(file)
 
+    nprob, neta, nL = length(ps), length(ηs), length(Ls)
+
+    prob_scales_mean = Matrix{Float64}(undef, nprob, nL)
+    prob_scales_std = Matrix{Float64}(undef, nprob, nL)
+    eta_scales_mean = Matrix{Float64}(undef, neta, nL)
+    eta_scales_std = Matrix{Float64}(undef, neta, nL)
+
+    file = h5open("data/entropy_scale_data.h5", "r")
     for (i,l) in enumerate(Ls)
         prob_scales_mean[:, i] .= read(file, "results_L=$l/prob_scales_mean")
         prob_scales_std[:, i] .= read(file, "results_L=$l/prob_scales_std")
@@ -40,14 +45,14 @@ let
     pp = plot(ps, prob_scales_mean,
          yerror=prob_scales_std, lw=1,
          xlabel=L"p", ylabel=L"S_1", 
-         title="Entanglement entropy for varying p, \eta=$η0",
+         title="Entanglement entropy for varying p, \\eta=$η0",
          label=string.(Ls'),
          legend_title=L"L")
     # Entanglement entropy scaling for varying η
     ep = plot(ηs, eta_scales_mean, 
          yerror=eta_scales_std, lw=1,
          xlabel=L"\eta", ylabel=L"S_1",
-         title="Entanglement entropy for varying η, p=$p0",
+         title="Entanglement entropy for varying \\eta, p=$p0",
          label=string.(Ls'),
          legend_title=L"L")
 
