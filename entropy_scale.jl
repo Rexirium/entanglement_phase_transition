ca3ea9f0e06c8f9d9fc92546fb41084f71e67de2
Rexirium <rexirium@qq.com>
2025-10-16 11:58:25 +0800

    ARGS version

--- entropy_scale.jl
+++ entropy_scale.jl
@@ -5,49 +5,42 @@ include("entropy_calc.jl")
 
 let
     # Parameters
-    Ls = 8:4:20
+    L = ARGS[1] == nothing ? 8 : parse(Int, ARGS[1])
+    T = ARGS[2] == nothing ? 4L : parse(Int, ARGS[2])
+    N = ARGS[3] == nothing ? 100 : parse(Int, ARGS[3])
+
     p0, η0 = 0.5, 0.5
     ps = 0.0:0.05:1.0
     ηs = 0.0:0.05:1.0
-    nprob, neta, nL = length(ps), length(ηs), length(Ls)
+    nprob, neta = length(ps), length(ηs)
+    cutoff = 1e-12 * L^3
     
     # Store results
-    prob_scales_mean = zeros(nprob, nL)
-    prob_scales_std = zeros(nprob, nL)
-    eta_scales_mean = zeros(neta, nL)
-    eta_scales_std = zeros(neta, nL)
-
-    # Create tasks array for all parameter combinations
-    tasks_prob = [(i,j) for i in 1:nprob, j in 1:nL]
-    tasks_eta = [(i,j) for i in 1:neta, j in 1:nL]
+    prob_scales_mean = zeros(nprob)
+    prob_scales_std = zeros(nprob)
+    eta_scales_mean = zeros(neta)
+    eta_scales_std = zeros(neta)
 
     # Calculate probability scaling
-    for task in vec(tasks_prob)
-        i, j = task
-        l = Ls[j]
-	cutoff = l^3 * 1e-12
-        prob_scales_mean[i,j], prob_scales_std[i,j] = 
-            entropy_mean_multi(l, 4l, ps[i], η0, l÷2; 
-		numsamp=100, cutoff=cutoff, ent_cutoff=cutoff,  retstd=true)
-        println("L=$l, p=$(round(ps[i],digits=2)), η=0.5 done")
+    for i in 1:nprob
+        prob_scales_mean[i], prob_scales_std[i] = 
+            entropy_mean_multi(L, T, ps[i], η0, L÷2; 
+		    numsamp=N, cutoff=cutoff, ent_cutoff=cutoff,  retstd=true)
+        println("L=$L, p=$(round(ps[i],digits=2)), η=0.5 done with $N samples.")
     end
 
     # Calculate eta scaling
-    for task in vec(tasks_eta)
-        i, j = task
-        l = Ls[j]
-	cutoff = l^3 * 1e-12
-        eta_scales_mean[i,j], eta_scales_std[i,j] = 
-            entropy_mean_multi(l, 4l, p0, ηs[i], l÷2; 
-		numsamp=100, cutoff=cutoff, ent_cutoff=cutoff, retstd=true)
-        println("L=$l, p=0.50, η=$(round(ηs[i],digits=2)) done")
+    for i in 1:neta
+        eta_scales_mean[i], eta_scales_std[i] = 
+            entropy_mean_multi(L, T, p0, ηs[i], L÷2; 
+		    numsamp=N, cutoff=cutoff, ent_cutoff=cutoff, retstd=true)
+        println("L=$L, p=0.50, η=$(round(ηs[i],digits=2)) done with $N samples.")
     end
 
     # Save data to HDF5 file
-    h5open("entropy_scale_test.h5", "w") do file
+    h5open("entropy_scale_test.h5", "cw") do file
         write(file, "ps", collect(ps))
         write(file, "ηs", collect(ηs))
-        write(file, "Ls", collect(Ls))
         
         write(file, "prob_scales_mean", prob_scales_mean)
         write(file, "prob_scales_std", prob_scales_std)
