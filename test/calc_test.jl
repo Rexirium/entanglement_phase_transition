6e0e100777778537c439d4546d4969fee1558e9a
Rexirium <rexirium@qq.com>
2025-12-10 21:48:30 +0800

    calc_test.jl rewrite

--- test/calc_test.jl
+++ test/calc_test.jl
@@ -1,10 +1,13 @@
+using MKL
+MKL.set_num_threads(1)
 include("../src/simulation.jl")
 
 let 
     L, T = 10, 40
     b = L ÷ 2
     p, η = 0.5, 0.5
-    N = 100
+    N = length(ARGS) == 0 ? 100 : parse(Int, ARGS[1])
 
-    @timev entropy_mean_multi(L, T, p, η; numsamp=N, cutoff=1e-14,  retstd=true, restype=Float64)
+    mean, std = @timev entropy_mean_multi(L, T, p, η; numsamp=N, cutoff=1e-14,  retstd=true, restype=Float64)
+    println("Entropy: $mean ± $std, with $T timesteps and $N samples.")
 end
