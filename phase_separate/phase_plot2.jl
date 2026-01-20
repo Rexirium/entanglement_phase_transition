b913dac2d5e7e1e3ca0117ffbcb3d4a09bd5a181
Rexirium <rexirium@qq.com>
2026-01-20 15:29:33 +0800

    phase plot using entrcorr data

---
+++ phase_separate/phase_plot2.jl
@@ -0,0 +1,96 @@
+using MKL
+using LinearAlgebra
+using Interpolations
+using Plots, LaTeXStrings
+using HDF5
+
+function linregress(xs, ys)
+    n = length(xs)
+    A = [ones(n) xs]
+    coeffs = A \ ys
+    return coeffs[2]
+end
+
+function linregress(xs, ys, yerrs)
+    n = length(xs)
+    ws = normalize(1 ./(yerrs .^ 2), 1)
+    W = diagm(sqrt.(ws))
+    A = [ones(n) xs]
+    Aw = W * A
+    yw = W * ys
+    coeffs = Aw \ yw
+    return coeffs[2]
+end
+
+let 
+    L1, dL, L2 = 4, 2, 20
+    nprob, neta = 21, 20
+
+    file = h5open("data/entrcorr_avg_L$(L1)_$(dL)_$(L2)_$(nprob)x$(neta).h5", "r")
+    type_str = read(file, "datatype")
+    ps = read(file, "params/ps")
+    ηs = read(file, "params/ηs")
+    Ls = read(file, "params/Ls")
+
+    type = eval(Meta.parse(type_str))
+
+    nL = length(Ls)
+    entropy_datas = Array{type}(undef, nprob, neta, nL)
+    entropy_error = Array{type}(undef, nprob, neta, nL)
+    for (i,l) in enumerate(Ls)
+        entropy_datas[:, :, i] .= read(file, "L_$l/entr_means")
+        entropy_error[:, :, i] .= read(file, "L_$l/entr_stds")
+    end
+    close(file)
+
+    indices = zeros(type, nprob, neta)
+
+    for i in 1:nprob
+        for j in 1:neta
+            xs = log.(Ls .+ 1e-10)
+            ys = log.(abs.(entropy_datas[i, j, :] .+ 1e-10))
+            yerrs = abs.(entropy_error[i, j, :] ./ entropy_datas[i, j, :])
+            index = linregress(xs, ys)
+            #=
+            if (abs(index) > 1.3 && j <= 3) || isnan(index)
+                indices[i, j] = 0.0
+            else
+                indices[i, j] = index
+            end
+            =#
+            indices[i, j] = isnan(index) ? 0.0 : index
+        end
+    end
+
+    p_knots = range(ps[1], ps[end], length=nprob)
+    η_knots = range(ηs[1], ηs[end], length=neta)
+    entropy_itp = cubic_spline_interpolation((p_knots, η_knots), indices)
+
+    p_fine = range(ps[1], ps[end], length=10*nprob)
+    η_fine = range(ηs[1], ηs[end], length=10*neta)
+    indices_fine = [entropy_itp(p, η) for p in p_fine, η in η_fine]
+
+    
+    heatmap(p_fine, η_fine, indices_fine'; 
+            xlabel=L"p", ylabel=L"\eta",
+            title="Entropy scaling index",
+            titlefontsize=14,
+            colorbar_title="index",
+            framestyle=:box, dpi=800
+            )
+    #savefig("phase_separate/entropy_scaling_index_modified.png")
+    #=
+    p0, η0 = 0.75, 0.0
+    pidx = findfirst(x -> x == p0, ps)
+    ηidx = findfirst(x -> x == η0, ηs)
+    data = abs.(entropy_datas[pidx, ηidx, :])
+    err = abs.(entropy_error[pidx, ηidx, :]./data)
+    println(data)
+    plot(log.(Ls), log.(data), 
+        yerror=err,
+         marker=:o, lw=2,
+         xlabel=L"L", ylabel="Entropy",
+         title="Entropy scaling at p=0.75, η=0.0",
+         label="data")
+    =#
+end
--- test/entropy_evolve_test.jl
+++ test/entropy_evolve_test.jl
@@ -10,18 +10,26 @@ ITensors.BLAS.set_num_threads(1)
 ITensors.Strided.set_num_threads(1)
 
 let 
-    L, T = 14, 140
-    p, η = 0.2, 0.8
+    L = 20
+    T = 12L
+    p, η = 0.6,0.4
     b = L ÷ 2
     
     ss = siteinds("S=1/2", L)
     psi = MPS(ss, "Up")
 
     obs = EntropyObserver{Float64}(b; n=1)
-    @timev mps_evolve!(psi, T, p, η, obs; cutoff=eps(Float64))
+    @timev mps_evolve!(psi, T, p, η, obs; cutoff=1e-14, maxdim=10*L)
+
+    entr_mean = mean(obs.entropies[2L+1:end])
+    entr_std = stdm(obs.entropies[2L+1:end], entr_mean; corrected=false)
+    turncerr_floor = (1e-14)*(T*L/2)
+
+    println("Entanglement Entropy at L = $L, p=$p, η=$η : $entr_mean ± $entr_std")
     
-    plot(0:T, obs.truncerrs; lw = 1.5, c=:red, xaxis=L"t", yaxis="err", label=L"\epsilon_\mathrm{tot}", legend_position=:bottomright)
-    pt = plot!(twinx(), 0:T, obs.maxbonds; lw=2, yaxis="max bond", label=L"D_\mathrm{max}", legend_position=:topright)
+    pt = plot(0:T, obs.truncerrs; lw = 1.5, c=:red, xaxis=L"t", yaxis="err", label=L"\epsilon_\mathrm{tot}", legend_position=:bottomright)
+    plot!(0:T, fill(turncerr_floor, T+1); lw=1.5, ls=:dash, c=:black, label="trunc err floor")
+    plot!(twinx(), 0:T, obs.maxbonds; lw=2, yaxis="max bond", label=L"D_\mathrm{max}", legend_position=:topright)
 
     pe = plot(0:T, obs.entropies, lw = 1.5, framestyle=:box, xlabel=L"t", ylabel="entropy", label=L"S_\mathrm{vN}(t)")
     
